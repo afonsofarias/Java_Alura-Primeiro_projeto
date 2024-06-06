4e18c557761048b8192be430e0d7a098ba56805c
afonsofarias <afonsojansen@gmail.com>
2024-06-06 16:06:58 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.outprintln("Esse e o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+
+
+    }
+}
